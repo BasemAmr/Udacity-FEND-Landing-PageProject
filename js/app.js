e4e887eede11242239a88f2e6468a45bc8e30eec
BasemAmr <67838239+BasemAmr@users.noreply.github.com>
2020-07-05 09:31:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@
  * 
 */
 
+const navbar = document.getElementById('navbar__list'),
+
+      sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
@@ -25,7 +28,30 @@
  * 
 */
 
+// Check if the section in the viewport
 
+function isInViewport() {
+
+    let sectionNum = sections[0];
+
+    let rectValue = 1000000;
+
+    for ( let section of sections ) {
+
+        let rect = section.getBoundingClientRect();
+
+        if ( rect.top > -300 & rect.top < rectValue ) {
+
+            rectValue = rect.top;
+
+            sectionNum = section;
+      }
+
+    }
+
+    return sectionNum;
+
+};
 
 /**
  * End Helper Functions
@@ -35,11 +61,73 @@
 
 // build the nav
 
+function createNavLists(){
+
+  for ( let section of sections ) {
+
+    let lists = document.createElement('li');
+
+    lists.className = 'menu__link';
+
+    lists.dataset.nav = section.id;
+
+    lists.textContent = lists.dataset.nav;
+
+    navbar.appendChild(lists);
+
+  }
+
+}
 
 // Add class 'active' to section when near top of viewport
 
 
+function addActiveClass(){
+
+  window.addEventListener('scroll', function(){
+
+    // Add Active Class To Section
+
+    let _section = isInViewport();
+
+    _section.classList.add('active-section');
+
+    for ( let section of sections ) {
+
+      if ( section.id != _section.id && section.classList.contains('active-section') ) {
+
+        section.classList.remove('active-section');
+
+      }
+
+    }
+
+    // Add Active Class To Navigation Links
+
+    const links = document.querySelectorAll('menu__link');
+
+    let _link = document.querySelector(`li[data-nav='${_section.id}']`);
+
+    _link.classList.add('active-link');
+
+    for ( let link of links ) {
+
+      if ( link.dataset.nav != _link.dataset.nav && link.classList.contains('active-link') ) {
+        
+        link.classList.remove('active--link');
+      
+      }
+
+    }
+
+  });
+
+}
+
+
 // Scroll to anchor ID using scrollTO event
+
+
 
 
 /**
